9a7be7245444776da47b7b733f3890fba87da77f
arcanix2509 <arcanixsupport2509@gmail.com>
2026-08-22 09:45:31 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { 
+  getAuth, 
+  GoogleAuthProvider, 
+  signInWithPopup, 
+  signOut, 
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC8DIXVcymOd3-ImBuMjRfZ_u6VC4Iapkw",
@@ -16,10 +24,17 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export { signInWithPopup, signOut, onAuthStateChanged };
 
-export const ADMIN_EMAIL = "your-admin-email@gmail.com";
+export { 
+  signInWithPopup, 
+  signOut, 
+  onAuthStateChanged, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword 
+};
+
+// ⚠️ ADMIN EMAIL YAHAN APNA RAKHO:
+export const ADMIN_EMAIL = "arcanixsupport2509@gmail.com";
+
 export const IMGBB_API_KEY = "0298c2843fc0f575928ba0562d0fb697";
-
-// ⚡ Gemini API Key (Replaced)
 export const GEMINI_API_KEY = "AQ.Ab8RN6Ii8UOz6k3MjH_1LBGtFkl22QFHqU1-qCCksUfbg2n9GQ";
